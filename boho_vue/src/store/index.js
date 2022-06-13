83b68be15d4b99b2b51d1b60bffd5af3ec310507
ShaineWarren <mwarren86@gmail.com>
2022-06-12 21:07:27 -0700

    working on cart

--- boho_vue/src/store/index.js
+++ boho_vue/src/store/index.js
@@ -1,11 +1,40 @@
 import { createStore } from 'vuex'
 
+// refrence used: https://bestofvue.com/repo/connor11528-vuex-shopping-cart
+// https://codesource.io/build-a-shopping-cart-with-vue-vuex-vue-material-design/
+// https://www.digitalocean.com/community/tutorials/how-to-build-a-shopping-cart-with-vue-3-and-vuex
 export default createStore({
   state: {
+    cart: {
+      items: [],
+    },
+    isAuthenticated: false,
+    token: '',
+    isLoading: false,
   },
   getters: {
   },
   mutations: {
+    initializeStore(state) {
+      // checks if there is already cart
+      if (localStorage.getItem('cart')) {
+        state.cart =  JSON.parse(localStorage.getItem('cart'))
+      // sets up a cart if not
+      } else {
+        localStorage.setItem('cart', JSON.stringify(state.cart))
+      }
+    },
+    addToCart(state, item) {
+      // checks if the item is already in the cart and adds to the total quantity
+      const exists = state.cart.items.filter(i => i.product.id === item.product.id)
+      if (exists.length) {
+        exists[0].quantity = parseInt(exists[0].quantity) + parseInt(item.quantity)
+      } else {
+        state.cart.items.push(item)
+      }
+      localStorage.setItem('cart', JSON.stringify(state.cart))
+      
+    }
   },
   actions: {
   },
